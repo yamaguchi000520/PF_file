d1731f3bd6d90180fcda6ad96a13f519e54e8025
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2022-12-05 12:40:09 +0000

    add.genre_index,edit,controller

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,39 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.page(params[:page]).per(10)
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルの登録に成功しました。"
+      redirect_to request.referer
+    else
+      @genres = Genres.page(params[:page]).per(10)
+      flash[:notice] = "ジャンルの登録に失敗しました。"
+      render :index
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "変更が完了しました。"
+      redirect_to admin_genres_path
+    else
+      flash[:notice] = "ジャンル名を入力してください。"
+      render :edit
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+    email: 'test@test.com',
+    password: '123456',
+)
+
+# ジャンルseed
+Genre.create!(
+  [
+    {
+      name: 'ビール'
+    },
+    {
+      name: 'ウイスキー'
+    },
+    {
+      name: 'ブランデー'
+    },
+    {
+      name: '焼酎'
+    },
+    {
+      name: 'サワー'
+    },
+    {
+      name: 'ワイン'
+    },
+    {
+      name: '日本酒'
+    },
+    {
+      name: 'その他'
+    }
+  ]
+)
+
+#Customer
+Customer.create!(
+  [
+    {
+        name: 'ラーメン好き太郎',
+        introduction: '初めまして、ラーメン好きの人です。',
+        email: 'i@i',
+        password: 'iiiiii'
+    }
+   ]
+)
