3974e2c78046b9e902ba66942d36efc2ef1bf80b
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2022-12-10 03:19:22 +0000

    fix.favorite_ajax

--- app/controllers/public/sake_comments_controller.rb
+++ app/controllers/public/sake_comments_controller.rb
@@ -1,11 +1,10 @@
 class Public::SakeCommentsController < ApplicationController
 
   def create
-    sake = Sake.find(params[:sake_id])
+    @sake = Sake.find(params[:sake_id])
     comment = current_customer.sake_comments.new(sake_comment_params)
     comment.sake_id = sake.id
     comment.save
-    redirect_to sake_path(sake)
   end
 
   def edit
@@ -16,8 +15,8 @@ class Public::SakeCommentsController < ApplicationController
   end
 
   def destroy
-    SakeComment.find(params[:id]).destroy
-    redirect_to sake_path(params[:sake_id])
+    @sake = SakeComment.find(params[:sake_id])
+    @sake.destroy
   end
 
   private
