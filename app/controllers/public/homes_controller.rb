27447a1e508a1cf6e094645e93243180d1b71b50
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2022-12-19 05:50:04 +0000

    fix.top_layout,sake_image

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @genres = Genre.all
+    @sakes = Sake.all
   end
 
   def about
--- app/models/sake.rb
+++ app/models/sake.rb
@@ -6,21 +6,24 @@ class Sake < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :sake_comments, dependent: :destroy
 
-  has_one_attached :image
+  has_one_attached :sake_image
 
   def favorited_by?(customer)
     favorites.exists?(customer_id: customer.id)
   end
 
-
-  def get_sake_image(width,height)
-    if image.attached?
-      image.variant(resize_to_limit: [width, height]).processed
-    else
-     file_path = Rails.root.join('app/assets/images/no_image.jpg')
-     image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
-    end
+  def get_sake_image
+    (sake_image.attached?) ? sake_image : 'no_image.jpg'
   end
+
+  # def get_sake_image(width,height)
+  #   if image.attached?
+  #     image.variant(resize_to_limit: [width, height]).processed
+  #   else
+  #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+  #     image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
+  #   end
+  # end
 
   def self.looks(search, word)
     if search == "perfect_match"
