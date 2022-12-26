bba1b397e7655770730a42f010ab05b4fe37ad56
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2022-12-26 05:18:50 +0000

    delete.public_genre_controller,index

--- app/controllers/public/genres_controller.rb
+++
@@ -1,7 +0,0 @@
-class Public::GenresController < ApplicationController
-  def index
-    @genre = Genre.find_by(:id params[id])
-    @genres = Genre.all
-    @sake = @sake.new
-  end
-end
--- test/controllers/public/genres_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Public::GenresControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get public_genres_index_url
-    assert_response :success
-  end
-end
