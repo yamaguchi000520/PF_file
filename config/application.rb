66f6cabdf705d5fa927bdfcf0efe41b3dd6eb000
kei yamaguchi <kei.yamaguchi00520@gmail.com>
2023-01-27 11:14:56 +0000

    fix.ja.yml

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module PfApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
   end
 end
